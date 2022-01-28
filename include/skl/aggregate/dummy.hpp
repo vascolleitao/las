fe77817127080d43507454f95c0fb91a2701e2f0
Vasco Leitao <vascolleitao@gmail.com>
2022-01-28 22:20:57 +0000

    Correct clang format

--- include/skl/aggregate/dummy.hpp
+++ include/skl/aggregate/dummy.hpp
@@ -5,30 +5,36 @@
 namespace skl::iterator
 {
   template<typename Data_t>
-  struct dummy : std::iterator<
-                        std::random_access_iterator_tag,    // iterator_category
-                        Data_t,                             // value_type
-                        std::ptrdiff_t ,                    // difference_type
-                        Data_t*,                            // pointer
-                        Data_t&                             // reference
-  >
+  struct dummy : std::iterator<std::random_access_iterator_tag,// iterator_category
+                   Data_t,// value_type
+                   std::ptrdiff_t,// difference_type
+                   Data_t*,// pointer
+                   Data_t&// reference
+                   >
   {
 
-    explicit dummy(Data_t* ptr) : m_ptr(ptr) {}
+    explicit dummy(Data_t* ptr)
+      : m_ptr(ptr)
+    {
+    }
 
     Data_t& operator*() const { return *m_ptr; }
     Data_t* operator->() { return m_ptr; }
     dummy& operator++() { return *this; }
-    dummy operator++(int) { dummy tmp = *this; return tmp; }
+    dummy operator++(int)
+    {
+      dummy tmp = *this;
+      return tmp;
+    }
     dummy& operator+=(const long int) { return *this; }
-    friend bool operator== ([[maybe_unused]]const dummy& lhs, [[maybe_unused]]const dummy& rhs) { return true; };
-    friend bool operator!= ([[maybe_unused]]const dummy& lhs, [[maybe_unused]]const dummy& rhs) { return true; };
+    friend bool operator==([[maybe_unused]] const dummy& lhs, [[maybe_unused]] const dummy& rhs) { return true; };
+    friend bool operator!=([[maybe_unused]] const dummy& lhs, [[maybe_unused]] const dummy& rhs) { return true; };
 
   private:
     Data_t* m_ptr;
   };
 
-} // namespace skl::iterator
+}// namespace skl::iterator
 
 
 namespace skl::aggregate
@@ -42,7 +48,8 @@ namespace skl::aggregate
 
     explicit dummy(Data_t&& var)
       : var_(std::forward<Data_t>(var))
-    {}
+    {
+    }
 
     iterator begin()
     {
@@ -54,7 +61,7 @@ namespace skl::aggregate
       return iterator(&var_);
     }
   };
-} // skl::aggregate
+}// namespace skl::aggregate
 
 namespace skl
 {
@@ -64,4 +71,4 @@ namespace skl
   {
     return aggregate::dummy<Data_t>(std::forward<Data_t>(var));
   }
-} // skl
+}// namespace skl
